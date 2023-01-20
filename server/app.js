f5869798e0f4da7c621cacdb882b86a02bd1cfb2
Sujan Maharjan <87776506+sujanW0W@users.noreply.github.com>
2023-01-20 22:37:49 +0545

    Creation Completed.

--- server/app.js
+++ server/app.js
@@ -1,22 +1,40 @@
 require("dotenv").config()
 require("express-async-errors")
 
+//extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
 const express = require("express")
 const app = express()
-
-const errorHandler = require("./middlewares/errorHandler")
-const notFound = require("./middlewares/notFound")
-const authentication = require("./middlewares/authentication")
 
 //DB
 const connectDB = require("./DB/connectDB")
 
-//routes
+const authentication = require("./middlewares/authentication")
+
+//routers
 const usersRouter = require("./routes/usersRouter")
 const productsRouter = require("./routes/productsRouter")
 
+//error handling
+const errorHandler = require("./middlewares/errorHandler")
+const notFound = require("./middlewares/notFound")
+
 //middlewares
+app.set('trust proxy', 1)
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    })
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.use("/api/v1/users", usersRouter)
 app.use("/api/v1/products", authentication, productsRouter)
